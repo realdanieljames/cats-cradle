f4559d0147f88c00d376487e989ca0e8fe1f3898
DANIEL JAMES <daniel.james@codeimmersives.com>
2020-06-25 12:18:52 -0400

    completed all 7 tasks

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 function challengeBanner(n) {
     return "######### Challenge " + n + " ######### " 
 }
-
+//__________________________________________________________________________________________________//
+//__________________________________________________________________________________________________//
 
 console.log(challengeBanner(1))
 function printOnlyVowels(string){
@@ -21,10 +22,26 @@ let i = 0
 } 
 printOnlyVowels("Regular expressions are for term 2.")
 //__________________________________________________________________________________________________//
+//__________________________________________________________________________________________________//
 
 
 
 console.log(challengeBanner(2))
+function First5Vowels(str) {
+    let count =  0
+    const vowels = 'aeiou';
+    let i = 0;
+    while (i < str.length) {
+        if (vowels.includes(str[i]) && count < 5 ) {
+            console.log(str[i]);
+            count = count + 1;
+        }
+        i = i + 1;
+    }
+}
+First5Vowels("Regular expressions are for term 2.")
+First5Vowels('hello')
+//__________________________________________________________________________________________________//
 //__________________________________________________________________________________________________//
 
 
@@ -41,12 +58,83 @@ function everyThirdCharacter(string){
 }
 everyThirdCharacter("I am the alfalfa and the omelette.")
 //__________________________________________________________________________________________________//
-console.log(challengeBanner(3))
-function first4Characters(string, indexNum){
-    let i = indexNum
-    while()
+//__________________________________________________________________________________________________//
+
+
+
+
+console.log(challengeBanner(4))
+function firstFourCharacters(str, start){
+    let count = 0;
+    let i = start;
+    while (i < str.length) {
+        if (count < 4) {
+            console.log(str[i]);
+            count =  count + 1;
+        }
+        i = i + 1;
+    }
 }
-first4Characters("Oh hi, I didn't see you there. Welcome.", 4)
-first4Characters("Oh hi, I didn't see you there. Welcome.", 38)
+firstFourCharacters("Oh hi, I didn't see you there. Welcome.", 4 )
+firstFourCharacters("Oh hi, I didn't see you there. Welcome.", 36 )
+//__________________________________________________________________________________________________//
+//__________________________________________________________________________________________________//
 
 
+
+console.log(challengeBanner(5))
+function indexOfU(str) {
+    let i = 0
+    while(i < str.length) {
+        if(str[i] === 'u') {
+            console.log(i)
+        }
+        i = i + 1;
+    }
+}
+indexOfU('You picked the wrong house, bub.')
+//__________________________________________________________________________________________________//
+//__________________________________________________________________________________________________//
+
+
+
+console.log(challengeBanner(6))
+function firstIndexOfU(str){
+    let found = false
+    let i = 0
+    while(i < str.length) {
+        if(str[i] === 'u' && found === false) {
+            console.log(i)
+
+            found = true
+        }
+        i = i + 1;
+    }
+}
+firstIndexOfU('You picked the wrong house, bub.')
+//__________________________________________________________________________________________________//
+//__________________________________________________________________________________________________//
+
+
+
+
+console.log(challengeBanner(7))
+function firstIndexOfU2(str){
+    let found = false
+    let i = 0
+    while(i < str.length) {
+        if(str[i] === 'u' && found === false) {
+            console.log(i)
+
+            found = true
+        }
+        i = i + 1;
+    }
+    if (found === false) {
+        console.log(-1)
+    }
+}
+firstIndexOfU2('You picked the wrong house, bub.')
+firstIndexOfU2("I'm Canadian")
+//__________________________________________________________________________________________________//
+//__________________________________________________________________________________________________//
